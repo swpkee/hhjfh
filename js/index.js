d9243499ea1f791eab929242cd49d0b486e0aa0b
swpkee <aodzaza113@gmail.com>
2020-10-17 20:58:38 +0700

    DetellScarch

--- js/index.js
+++ js/index.js
@@ -117,9 +117,42 @@ $("#carouselBig ons-carousel-item").click(function () {
 })
 
 
-function getmovieSearch() {
-  console.log($("#searchInput").val());
+
+
+
+
+function getmoviefromSearch() {
+  const searchText = $("#searchInput").val()
+  const newsearchText = searchText.replace(/ /g, "");
+  $("#searchItem").empty();
+  db.collection("movies").get().then((querySnapshot) => {
+    querySnapshot.forEach((doc) => {
+      const titlemovie = doc.data().title;
+      const newtitleMovie = titlemovie.replace(/ /g, "");
+      if (newtitleMovie.toLowerCase().indexOf(newsearchText.toLowerCase()) != -1) {
+        const eiei = `
+        <ons-row class="rowmagin"> 
+        <ons-col>
+        <img src="${doc.data().posterURL}" style="width:90%;height:100%;">
+           </div>
+        </ons-col>
+        <ons-col>
+              <span class="list-item__title">Us</span><br>
+              <span class="list-item__subtitle">Vestibulum congue placerat magna in ornare. In hac habitasse platea dictumst.
+                 Mauris rhoncus iaculis magna, at molestie mauris gravida sagittis...
+                </span>
+                 <p class="rating">Ratings</p>
+                        <div class="starrate">
+                            <ons-icon class="starspace" icon="md-star"></ons-icon>
+                            <ons-icon class="starspace" icon="md-star"></ons-icon>
+                            <ons-icon class="starspace" icon="md-star"></ons-icon>
+                            <ons-icon class="starspace" icon="md-star"></ons-icon>
+                        </div>
+              </ons-col>
+      </ons-row>`
+        console.log(doc.data());
+        $("#searchItem").append(eiei)
+      }
+    });
+  });
 }
-
-
-
